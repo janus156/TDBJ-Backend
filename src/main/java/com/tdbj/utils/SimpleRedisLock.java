5ec78c64f1b909835208263b7c14501b46834e87
janus156 <1569361808@qq.com>
2023-02-11 19:41:22 +0800

    添加了lua脚本

--- src/main/java/com/tdbj/utils/SimpleRedisLock.java
+++ src/main/java/com/tdbj/utils/SimpleRedisLock.java
@@ -1,12 +1,23 @@
 package com.tdbj.utils;
 
+import cn.hutool.core.io.resource.ClassPathResource;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.StrUtil;
+import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript();
+        UNLOCK_SCRIPT.setLocation((Resource) new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     private String name;
     private StringRedisTemplate stringRedisTemplate;
 
@@ -33,14 +44,20 @@ public class SimpleRedisLock implements ILock {
         return Boolean.TRUE.equals(isSucceeded);
     }
 
+
+
+    /**
+     * 释放锁
+     */
     @Override
     public void unLock() {
-        String threadIdentifier = ID_PREFIX + Thread.currentThread().getId();
-        String threadIdentifierFromRedis = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 比较 锁中的线程标识 与 当前的线程标识 是否一致
-        if (StrUtil.equals(threadIdentifier, threadIdentifierFromRedis)) {
-            // 释放锁标识
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // 调用 Lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,  // SCRIPT
+                Collections.singletonList(KEY_PREFIX + name),   // KEY[1]
+                ID_PREFIX + Thread.currentThread().getId()    // ARGV[1]
+        );
     }
+
 }
+
